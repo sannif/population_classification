e44ff2a960bf9b8be834e85923ef253334b1dcc7
sannif <sannif96@gmail.com>
2016-12-04 23:56:32 -0500

    ca avance très doucement :)

--- script.R
+++ script.R
@@ -1,27 +1,52 @@
 #récupération des données
 NAm2 = read.table("NAm2.txt", header = T)
 
-#composantes principales
-pca = prcomp(NAm2[,-c(1:8)])
-pvar = pca$sdev/sum(pca$sdev)*100
-par(mfrow=c(1,2))
-plot(1:length(pvar), pvar, type="l", lwd=1.5, col="blue", xlab="composantes",
-ylab="pourcentage de variance expliquée")
-plot(1:length(pvar), cumsum(pvar), type="l", lwd=1.5, col="blue", xlab="composantes",
-      ylab="pourcentage de variance expliquée cumulée")
-par(mfrow=c(1,1))
-
 #séparation des données
 set.seed(12345)
 train = sample(1:nrow(NAm2), 400)
-gen = data.frame(Pop=NAm2$Pop, pca$x[,1:300])
+
+#composantes principales
+pca = prcomp(NAm2[train,-c(1:8)])
+pvar = pca$sdev/sum(pca$sdev)*100
+par(mfrow=c(1,2))
+plot(1:length(pvar), pvar, type="l", lwd=1.5, col="blue", xlab="composantes",
+     ylab="pourcentage de variance expliquée")
+plot(1:length(pvar), cumsum(pvar), type="l", lwd=1.5, col="blue", xlab="composantes",
+     ylab="pourcentage de variance expliquée cumulée")
+par(mfrow=c(1,1))
+
+ncomp = 300
+gen = data.frame(Pop=NAm2$Country[train], pca$x[,1:ncomp])
 
 require(nnet)
 #ajustement
-fit.log = multinom(Pop~., data=gen, subset = train)
+fit.log = multinom(Pop~., data=gen, MaxNWts=20000)
 #prédiction sur l'ensemble de test
-pred_probs = predict(fit.log, newdata = gen[-train,], type="probs")
-pred_names = attr(pred, 'dimnames')[[2]]
-preds = apply(pred, 1, function(x) pred_names[which.max(x)])
-mean(gen$Pop[-train]!=preds)
-table(gen$Pop[-train], pred_val)
+test_data = scale(NAm2[-train,-c(1:8)], pca$center, pca$scale) %*% pca$rotation
+test_data = as.data.frame(test_data[,1:ncomp])
+
+names(test_data) = names(gen)[-1]
+preds.log = predict(fit.log, newdata = test_data, type="class")
+mean(NAm2$Country[-train]!=preds.log)
+table(NAm2$Country[-train], preds.log)
+
+
+#LDA
+require(MASS)
+fit.lda = lda(Pop~.,data=gen)
+preds.lda = predict(fit.lda, newdata = test_data, type="response")$class
+lda.error = mean(NAm2$Country[-train] != preds.lda)
+table(NAm2$Country[-train], preds.log)
+
+#QDA
+require(MASS)
+fit.qda = qda(Pop~.,data=gen)
+preds.qda = predict(fit.qda, newdata = test_data, type="response")$class
+mean(NAm2$Pop[-train] != preds.lda)
+
+# Logistic regularized
+require(glmnet)
+x = model.matrix(Pop~., data=NAm2[train, -c(1,2,4:8)])[,-1]
+#grid = 10^seq (1.6,-2, length =1000)
+fit.log_ridge = glmnet(x=x, y=NAm2$Pop[train], alpha = 0, family="multinomial")
+
